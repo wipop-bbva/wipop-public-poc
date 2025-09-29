b242dc3fd409581fd4fbdc047c761ff3b5e5ed8d
Marco Antonio Rivas Salinas <marcoantonio.rivas@openpay.mx>
2025-09-29 11:54:24 -0600

    Deploy a sonatype con plugin

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,9 +1,35 @@
 jdk_tool = 'java-21'
-
+deploy_to_sonatype = 'true'
+is_github_repo = 'true'
 jte {
 	pipeline_template = "jar_library"
 }
 
 libraries {
 	maven
+}
+
+application_environments{
+    dev{name
+        bucket = ''
+        source = ''
+        destination = ''
+        secret_name = 'op-jenkins-secrets'
+        key_bucket = ''
+        key_path = ''
+        sign_apk = 'false'
+    }
+  sandbox {
+        bucket = ''
+        source = ''
+        destination = ''
+        secret_name = 'op-jenkins-secrets'
+    }
+  prod{
+        bucket = ''
+        source = ''
+        destination = ''
+        secret_name = 'op-jenkins-secrets'
+    }
+
 }
