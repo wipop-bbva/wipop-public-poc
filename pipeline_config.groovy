44d36dbfce1b4c6dbd23b1e46cbeafbada00502c
Marco Antonio Rivas Salinas <marcoantonio.rivas@openpay.mx>
2025-10-24 22:22:12 -0600

    Se agregan secretos

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -2,7 +2,8 @@ jdk_tool = 'java-21'
 deploy_to_sonatype = 'true'
 is_github_repo = 'true'
 maven_install = 'openpay-maven-3.9.6'
-
+sonatype_credentials_id = 'ossrh'
+secret_name = 'op-mx-dev-jenkins'
 
 jte {
 	pipeline_template = "jar_library"
